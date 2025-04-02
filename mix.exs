e21de0ab22a85c923a95f8bbb488e2114bba84c9
datnguyennnx <official.nguyendat@gmail.com>
2025-04-02 13:44:32 +0700

    docs(changelog): version 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Central.MixProject do
   def project do
     [
       app: :central,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
